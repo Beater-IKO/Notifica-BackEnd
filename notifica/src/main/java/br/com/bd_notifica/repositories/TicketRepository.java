f63f81b6fc225a4041e83779c0a1002691be423e
Carlos Daniel <144396126+carlosdaniel-20@users.noreply.github.com>
2025-04-09 21:36:03 -0300

    Correção da mistura do custom Factory com o JpaUtil

--- notifica/src/main/java/br/com/bd_notifica/repositories/TicketRepository.java
+++ notifica/src/main/java/br/com/bd_notifica/repositories/TicketRepository.java
@@ -1,16 +1,17 @@
 package br.com.bd_notifica.repositories;
 
+import br.com.bd_notifica.configs.CustomFactory;
 import br.com.bd_notifica.entities.Ticket;
-import br.com.bd_notifica.utils.JpaUtil;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
+import java.time.LocalDate;
 import java.util.List;
 
 public class TicketRepository {
 
     public Ticket salvar(Ticket ticket) {
-        EntityManager em = JpaUtil.getEntityManager();
+        EntityManager em = CustomFactory.getEntityManager();
         try {
             em.getTransaction().begin();
             em.persist(ticket);
@@ -22,18 +23,18 @@ public class TicketRepository {
     }
 
     public List<Ticket> listarTodos() {
-        EntityManager em = JpaUtil.getEntityManager();
+        EntityManager em = CustomFactory.getEntityManager();
         TypedQuery<Ticket> query = em.createQuery("FROM Ticket", Ticket.class);
         return query.getResultList();
     }
 
     public Ticket buscarPorId(Long id) {
-        EntityManager em = JpaUtil.getEntityManager();
+        EntityManager em = CustomFactory.getEntityManager();
         return em.find(Ticket.class, id);
     }
 
     public void deletar(Long id) {
-        EntityManager em = JpaUtil.getEntityManager();
+        EntityManager em = CustomFactory.getEntityManager();
         try {
             Ticket ticket = em.find(Ticket.class, id);
             if (ticket != null) {
@@ -46,36 +47,32 @@ public class TicketRepository {
         }
     }
 
-    // Atualizar um ticket
-public Ticket editar(Ticket ticket) {
-    EntityManager em = CustomFactory.getEntityManager();
-    try {
-        em.getTransaction().begin();
-        ticket = em.merge(ticket);
-        em.getTransaction().commit();
-        return ticket;
-    } finally {
-        em.close();
+    public Ticket editar(Ticket ticket) {
+        EntityManager em = CustomFactory.getEntityManager();
+        try {
+            em.getTransaction().begin();
+            ticket = em.merge(ticket);
+            em.getTransaction().commit();
+            return ticket;
+        } finally {
+            em.close();
+        }
     }
-}
 
-// Buscar por ID do aluno
-public List<Ticket> buscarPorAlunoId(Long alunoId) {
-    EntityManager em = CustomFactory.getEntityManager();
-    TypedQuery<Ticket> query = em.createQuery(
-        "SELECT t FROM Ticket t WHERE t.alunoId = :alunoId", Ticket.class);
-    query.setParameter("alunoId", alunoId);
-    return query.getResultList();
-}
+    public List<Ticket> buscarPorAlunoId(Long alunoId) {
+        EntityManager em = CustomFactory.getEntityManager();
+        TypedQuery<Ticket> query = em.createQuery(
+                "SELECT t FROM Ticket t WHERE t.alunoId = :alunoId", Ticket.class);
+        query.setParameter("alunoId", alunoId);
+        return query.getResultList();
+    }
 
-// Buscar por intervalo de datas
-public List<Ticket> buscarPorIntervalo(LocalDate inicio, LocalDate fim) {
-    EntityManager em = CustomFactory.getEntityManager();
-    TypedQuery<Ticket> query = em.createQuery(
-        "SELECT t FROM Ticket t WHERE t.dataCriacao BETWEEN :inicio AND :fim", Ticket.class);
-    query.setParameter("inicio", inicio);
-    query.setParameter("fim", fim);
-    return query.getResultList();
-}
-
+    public List<Ticket> buscarPorIntervalo(LocalDate inicio, LocalDate fim) {
+        EntityManager em = CustomFactory.getEntityManager();
+        TypedQuery<Ticket> query = em.createQuery(
+                "SELECT t FROM Ticket t WHERE t.dataCriacao BETWEEN :inicio AND :fim", Ticket.class);
+        query.setParameter("inicio", inicio);
+        query.setParameter("fim", fim);
+        return query.getResultList();
+    }
 }
