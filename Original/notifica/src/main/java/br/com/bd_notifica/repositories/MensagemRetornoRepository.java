71316fbf0ca771fd8789b7bccac312121e908368
Alex <burraum55@gmail.com>
2025-08-17 13:37:15 -0300

    me mata
    a minha vida e uma mentira

--- Original/notifica/src/main/java/br/com/bd_notifica/repositories/MensagemRetornoRepository.java
+++ Original/notifica/src/main/java/br/com/bd_notifica/repositories/MensagemRetornoRepository.java
@@ -1,5 +1,7 @@
 package br.com.bd_notifica.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,6 @@ import br.com.bd_notifica.entities.MensagemRetorno;
 
 @Repository
 public interface MensagemRetornoRepository extends JpaRepository <MensagemRetorno, Integer>{
+    List<MensagemRetorno> findByUserIgnoreCase(String user);
+    List<MensagemRetorno> findByTicketIgnoreCase(String ticket);
 }
