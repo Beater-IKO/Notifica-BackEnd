706c6c4d909a7047c64712267ac24ed889009a16
Alex <burraum55@gmail.com>
2025-08-17 13:26:18 -0300

    desculpa
    eu nao aguento mais commitar ctrl+s esquecidos 😭

--- Original/notifica/src/main/java/br/com/bd_notifica/repositories/CategoriaRepository.java
+++ Original/notifica/src/main/java/br/com/bd_notifica/repositories/CategoriaRepository.java
@@ -1,5 +1,7 @@
 package br.com.bd_notifica.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,6 @@ import br.com.bd_notifica.entities.Categoria;
 
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+    List<Categoria> findByNomeIgnoreCase(String nome);
+    List<Categoria> findByCategoriaIgnoreCase(String categoria);
 }
